0af65e52d5bb3a26ad0bb06c8d1115dad0752363
sergkhl <sergkhl@users.noreply.github.com>
2020-08-30 13:52:42 +0000

    deploy: 35e0184b7c75067f1f524c3b5616c3a3e29db8a5

---
+++ .lighthouserc.js
@@ -0,0 +1,26 @@
+// https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md
+
+module.exports = {
+  ci: {
+    collect: {
+      url: ['https://sergkhl.github.io/template-workflows-typescript-vue3/'],
+      // startServerCommand: 'rails server -e production',
+      numberOfRuns: 1,
+      staticDistDir: './',
+      // staticDistDir: null,
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      // assertions: {
+      //   'uses-rel-preload': 'off',
+      //   'uses-rel-preconnect': 'off',
+      // },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+      //   target: 'lhci',
+      //   serverBaseUrl: 'https://your-lhci-server-url.example.com',
+      //   token: 'Your *build token* goes here', // could also use LHCI_TOKEN variable instead
+    },
+  },
+}
