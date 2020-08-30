8f5e1e67d620bd0d98374131145e678df063bcd3
Sergio Khlopenkov <sergio.khlopenkov@gmail.com>
2020-08-30 17:56:47 +0300

    fix(github workflows): try #4

--- .github/lighthouse/lighthouserc.js
+++ .github/lighthouse/lighthouserc.js
@@ -9,13 +9,13 @@ module.exports = {
       staticDistDir: './dist',
       // staticDistDir: null,
     },
-    // assert: {
-    //   preset: 'lighthouse:recommended',
-    //   // assertions: {
-    //   //   'uses-rel-preload': 'off',
-    //   //   'uses-rel-preconnect': 'off',
-    //   // },
-    // },
+    assert: {
+      preset: 'lighthouse:recommended',
+      // assertions: {
+      //   'uses-rel-preload': 'off',
+      //   'uses-rel-preconnect': 'off',
+      // },
+    },
     upload: {
       target: 'temporary-public-storage',
       //   target: 'lhci',
